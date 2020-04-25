be2d4b669b829d77b1097acedd950380041b5d3a
陈长青 <greenchen@fglai.com>
2020-04-25 12:04:10 +0800

    1

--- gitbook.sh
+++ gitbook.sh
@@ -2,12 +2,12 @@ rm -r -f _book
 gitbook build
 git checkout master
 git add .
-git commit -m $1
+git commit -m 'build'
 git push -u origin master
 git checkout gh-pages
 cp -r _book/* .
 git add .
-git commit -m $1
+git commit -m 'build'
 git push -u origin gh-pages
 git checkout master
 echo "提交OK"
